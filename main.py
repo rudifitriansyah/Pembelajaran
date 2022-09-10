2276a991f5bdad0dbfa578c7512ed5b9a938382b
rudifitriansyah <113221869+rudifitriansyah@users.noreply.github.com>
2022-09-10 09:35:18 +0700

    commit pertama

---
+++ main.py
@@ -0,0 +1,17 @@
+nama="Rudi Fitriansyah"
+print(nama)
+
+angka_bulat =1,3
+print("tipe int",angka_bulat)
+
+tipe_boolean = False
+print(tipe_boolean)
+
+tipe_list = ['ahmad','budi','samsul']
+print(tipe_list)
+print(tipe_list[2])
+
+tipe_set = {1,1,2,2,3,3}
+print(tipe_set)
+
+pertambahan = 2+3+300+32+54
